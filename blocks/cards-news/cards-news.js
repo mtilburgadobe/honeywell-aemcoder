92be1410880f40c1e782d97c3ce58bfd02f87538
AEMY Studio <noreply@aemy.studio>
2026-04-16 09:21:59 +0000

    Add cards-news block
    Automated migration updates generated by Experience Modernization Agent.
    
    Changed files (2):
    - blocks/cards-news/cards-news.css
    - blocks/cards-news/cards-news.js

--- blocks/cards-news/cards-news.js
+++ blocks/cards-news/cards-news.js
@@ -7,14 +7,24 @@ export default function decorate(block) {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-news-card-image';
-      else div.className = 'cards-news-card-body';
+      const pic = div.querySelector('picture');
+      const img = div.querySelector('img');
+      if (div.children.length === 1 && (pic || (img && !div.querySelector('h1, h2, h3, h4, h5, h6, a')))) {
+        div.className = 'cards-news-card-image';
+      } else {
+        div.className = 'cards-news-card-body';
+      }
     });
     ul.append(li);
   });
-  ul.querySelectorAll('picture > img').forEach((img) => {
-    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
-    img.closest('picture').replaceWith(optimizedPic);
+  ul.querySelectorAll('img').forEach((img) => {
+    if (!img.closest('picture')) {
+      const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
+      img.replaceWith(optimizedPic);
+    } else {
+      const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
+      img.closest('picture').replaceWith(optimizedPic);
+    }
   });
   block.textContent = '';
   block.append(ul);
